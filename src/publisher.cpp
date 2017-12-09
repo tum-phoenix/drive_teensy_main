53d254b5aa5aed20fd4a0b160f629623fc1238a4
fabolhak <dev-faha@t-online.de>
2017-12-09 17:56:33 +0100

    update submodule and added keyValue message

--- src/publisher.cpp
+++ src/publisher.cpp
@@ -1,5 +1,7 @@
+#include "Arduino.h"
 #include <UAVCAN.hpp>
 #include <uavcan/protocol/debug/LogMessage.hpp>
+#include <uavcan/protocol/debug/KeyValue.hpp>
 
 using namespace uavcan;
 
@@ -7,8 +9,10 @@ static const unsigned NodeMemoryPoolSize = 8192;
 static constexpr uint32_t bitrate = 1000000;
 static constexpr uint32_t nodeID = 101;
 
+int blink_led = 16;
 Node<NodeMemoryPoolSize> *node;
 Publisher<protocol::debug::LogMessage> *logPublisher;
+Publisher<protocol::debug::KeyValue> *keyPublisher;
 
 
 ISystemClock& getSystemClock()
@@ -50,11 +54,14 @@ void setup()
   Serial.begin(9600);
   Serial.println("Setup");
 
+  pinMode(blink_led, OUTPUT);
+
   // Create a node
   node = new Node<NodeMemoryPoolSize>(getCanDriver(), getSystemClock());
   Serial.println("Application node created");
   // Create a publisher for LogMessages
   logPublisher = new Publisher<protocol::debug::LogMessage>(*node);
+  keyPublisher = new Publisher<protocol::debug::KeyValue>(*node);
 
   node->setNodeID(nodeID);
   node->setName("org.phoenix.publisher");
@@ -68,12 +75,18 @@ void setup()
   {
     Serial.println("Unable to initialize log message publisher!");
   }
+  if(keyPublisher->init() < 0)
+  {
+    Serial.println("Unable to initialize key message publisher!");
+  }
 
   node->setModeOperational();
 }
 
 void loop()
 {
+
+  digitalWrite(blink_led, HIGH);
   // Wait for frames 100ms, then do other stuff
   Serial.println("Application spinning");
   const int res = node->spin(uavcan::MonotonicDuration::fromMSec(1000));
@@ -85,7 +98,34 @@ void loop()
   protocol::debug::LogMessage msg;
   const int pres = logPublisher->broadcast(msg);
   if (pres < 0)
+  // send a very important log message to everyone
   {
     Serial.println("Error while broadcasting message");
+    protocol::debug::LogMessage msg;
+
+    msg.text = "TUM PHOENIX Robotics if(winCaroloCup){is awesome}else{sucks}!";
+
+    Serial.println("Start Broadcasting");
+    const int pres = logPublisher->broadcast(msg);
+    if (pres < 0)
+    {
+      Serial.println("Error while broadcasting log message");
+    }
   }
+
+  // send everyone the truth
+  {
+    protocol::debug::KeyValue msg;
+
+    msg.value = 42;
+    msg.key = "Solution to all Problems!";
+
+    const int pres = keyPublisher->broadcast(msg);
+    if (pres < 0)
+    {
+      Serial.println("Error while broadcasting key message");
+    }
+  }
+  digitalWrite(blink_led, LOW);
+  delay(1000);
 }
