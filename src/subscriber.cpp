87196f68a46a85dea4e4c3bc0a950d2850d6aff4
fabolhak <dev-faha@t-online.de>
2017-12-10 14:03:31 +0100

    added subscriber branch

--- src/subscriber.cpp
+++ src/subscriber.cpp
@@ -1,5 +1,6 @@
 #include <UAVCAN.hpp>
 #include <uavcan/protocol/debug/LogMessage.hpp>
+#include <uavcan/protocol/debug/KeyValue.hpp>
 
 using namespace uavcan;
 
@@ -10,6 +11,7 @@ static constexpr uint32_t nodeID = 100;
 
 Node<NodeMemoryPoolSize> *node;
 Subscriber<protocol::debug::LogMessage> *logSubscriber;
+Subscriber<protocol::debug::KeyValue> *keySubscriber;
 
 ISystemClock& getSystemClock()
 {
@@ -41,6 +43,15 @@ void logMessageCallback(const uavcan::protocol::debug::LogMessage& msg)
   Serial.println("Received log message");
 }
 
+void keyMessageCallback(const uavcan::protocol::debug::KeyValue& msg)
+{
+  Serial.println("Received key message!");
+  //Serial.print("Key:");
+  //Serial.println(msg.key);
+  Serial.print("Value:");
+  Serial.println(msg.value);
+}
+
 
 void setup()
 {
@@ -54,6 +65,7 @@ void setup()
 
   // Create a subsciber for LogMessages
   logSubscriber = new Subscriber<protocol::debug::LogMessage>(*node);
+  keySubscriber = new Subscriber<protocol::debug::KeyValue>(*node);
 
   // Start Node and then the subscriber
   if(node->start() < 0)
@@ -64,15 +76,24 @@ void setup()
   {
     Serial.println("Unable to start log message subscriber!");
   }
+  if(keySubscriber->start(keyMessageCallback) < 0)
+  {
+    Serial.println("Unable to start log message subscriber!");
+  }
+
 
   node->setModeOperational();
 }
 
+int ct = 0;
+
 void loop()
 {
   // Wait for frames 100ms, then do other stuff
-  Serial.println("Application start spinning");
-  const int res = node->spin(uavcan::MonotonicDuration::fromMSec(100));
+  Serial.print("Application spinning. Heartbeat counter: ");
+  Serial.println(ct);
+  ct++;
+  const int res = node->spin(uavcan::MonotonicDuration::fromMSec(1000));
   if (res < 0)
   {
       Serial.println("Error while spinning...");
