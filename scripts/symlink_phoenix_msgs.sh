df5d6d98af048990f82681a19d6735586aad3725
MaslinuPoimal <mykyta.denysov@gmail.com>
2019-01-27 16:38:32 +0100

    Add symlink script back

---
+++ scripts/symlink_phoenix_msgs.sh
@@ -0,0 +1,9 @@
+if [ ! -d ~/uavcan_vendor_spefic_types ]; then
+   	echo Creating folder ~/uavcan_vendor_specific_types
+	mkdir ~/uavcan_vendor_specific_types
+fi
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ABS_DIR=$(realpath $DIR/../lib/phoenix_msgs)
+UAVCAN_DIR=$(realpath ~/uavcan_vendor_specific_types)
+echo Symlinking Folder $ABS_DIR to $UAVCAN_DIR
+ln -s $ABS_DIR $UAVCAN_DIR
