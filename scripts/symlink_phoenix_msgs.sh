e945ac79f7977ee8d91543ee735a21d47207beea
MaslinuPoimal <mykyta.denysov@gmail.com>
2019-05-26 22:18:18 +0200

    Add ros package wrapper and execute the symlink script when building it

--- scripts/symlink_phoenix_msgs.sh
+++ scripts/symlink_phoenix_msgs.sh
@@ -5,5 +5,7 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ABS_DIR=$(realpath $DIR/../lib/phoenix_msgs)
 UAVCAN_DIR=$(realpath ~/uavcan_vendor_specific_types)
-echo Symlinking Folder $ABS_DIR to $UAVCAN_DIR
-ln -s $ABS_DIR $UAVCAN_DIR
+if [ ! -d ~/uavcan_vendor_spefic_types/phoenix_msgs ]; then
+        echo Symlinking Folder $ABS_DIR to $UAVCAN_DIR
+        ln -s $ABS_DIR $UAVCAN_DIR
+fi
